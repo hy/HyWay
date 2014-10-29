e3f2866d7212ab417bade85345904a170574b3ed
DrStephenRacunas <Stephen.A.Racunas@gmail.com>
2014-10-29 09:24:50 -0700

    Basic points tracking

--- TheApp.rb
+++ TheApp.rb
@@ -1464,7 +1464,7 @@ class TheApp < Sinatra::Base
         if r['Language'] == nil
           @Language = 'Bengali'
         else
-          @Language = d['Language']
+          @Language = r['Language']
         end #if
 
         content_scope = {'Department' => r['Department'], 
