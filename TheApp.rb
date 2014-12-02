46efeeafdc034654772ba2865a36655dc091f78f
DrStephenRacunas <Stephen.A.Racunas@gmail.com>
2014-12-02 14:07:04 -0800

    Try example super-basic uploader in haml

--- TheApp.rb
+++ TheApp.rb
@@ -595,7 +595,7 @@ end
     
 # Handle POST-request (Receive and save the uploaded file)
 post '/upload' do 
-  File.open('uploads/' + params['myfile'][:filename], "w") do |f|
+  File.open('./tmp/' + params['myfile'][:filename], "w") do |f|
     f.write(params['myfile'][:tempfile].read)
   end
   return "The file was successfully uploaded!"
